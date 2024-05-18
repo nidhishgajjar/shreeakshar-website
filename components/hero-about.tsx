40191aa88466d512836d50890ca847b8aba59cb1
Nidhish Gajjar <hacker@scigic.com>
2024-05-18 00:05:54 -0700

    Updated about image and layout

--- components/hero-about.tsx
+++ components/hero-about.tsx
@@ -8,9 +8,9 @@ export default function HeroAbout() {
 
       {/* Dark background */}
       <div className="absolute inset-0 bg-slate-900 pointer-events-none -z-10 mb-48 lg:mb-0 lg:h-[30rem]" aria-hidden="true">
-        <div className="w-full h-full" data-aos="fade">
+        {/* <div className="w-full h-full" data-aos="fade">
           <Image className="opacity-10 w-full h-full object-cover" src={HeroImage} width={1440} height={497} priority alt="Hero" />
-        </div>
+        </div> */}
       </div>
 
       <div className="relative max-w-6xl mx-auto px-4 sm:px-6">
@@ -23,7 +23,7 @@ export default function HeroAbout() {
 
           {/* Hero image */}
           <div className="flex justify-center items-center" data-aos="fade-up" data-aos-delay="100">
-            <Image className="mx-auto" src={IntroImage} width={1024} height={576} priority alt="About intro" />
+            <Image className="mx-auto" src={HeroImage} width={900} priority alt="About intro" />
           </div>
 
         </div>
